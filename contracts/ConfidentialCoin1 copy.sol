46d43dbfd887b0233fd636cee3cf2c03e2385f04
MayraAyalaai <MichelleHolloway1969124@hotmail.com>
2025-09-02 15:23:30 +0800

    create test confidenttial coins

---
+++ contracts/ConfidentialCoin1 copy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.24;
+
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {ConfidentialFungibleToken} from "@openzeppelin/confidential-contracts/token/ConfidentialFungibleToken.sol";
+import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
+
+contract ConfidentialCoin1 is ConfidentialFungibleToken, SepoliaConfig {
+    constructor() ConfidentialFungibleToken("cCoin1", "CC1", "") {}
+
+    function mint(address to, uint64 amount) public {
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        _mint(to, encryptedAmount);
+    }
+}
