a7e0ba6ab2cb71b7208771b189b1322cbec13948
MayraAyalaai <MichelleHolloway1969124@hotmail.com>
2025-09-17 23:27:01 +0800

    deploy and test

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
+  // Deploy FHECounter first
   const deployedFHECounter = await deploy("FHECounter", {
     from: deployer,
     log: true,
   });
-
   console.log(`FHECounter contract: `, deployedFHECounter.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
+func.id = "deploy_fhe_counter"; // id required to prevent reexecution
 func.tags = ["FHECounter"];
